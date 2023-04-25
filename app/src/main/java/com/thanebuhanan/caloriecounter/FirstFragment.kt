c3926e7ebf99f8ff133e344f1c07ba10b9c732e5
ThaneBuhanan <thanebuhanan@gmail.com>
2023-04-25 15:17:32 -0700

    Splash screen working.

--- app/src/main/java/com/thanebuhanan/caloriecounter/FirstFragment.kt
+++ app/src/main/java/com/thanebuhanan/caloriecounter/FirstFragment.kt
@@ -2,22 +2,13 @@ package com.thanebuhanan.caloriecounter
 
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.thanebuhanan.caloriecounter.databinding.FragmentFirstBinding
-import data.Network
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
 
-
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
---
+++ app/src/main/java/com/thanebuhanan/caloriecounter/SplashFragment.kt
@@ -0,0 +1,44 @@
+package com.thanebuhanan.caloriecounter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.thanebuhanan.caloriecounter.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            delay(5000)
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToFirstFragment())
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (requireActivity() as AppCompatActivity).supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (requireActivity() as AppCompatActivity).supportActionBar!!.show()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
