954b5af152ea0c6d4bfb7e6965aae5d2c1479e02
ThaneBuhanan <thanebuhanan@gmail.com>
2023-05-05 15:39:29 -0700

    Deleted unused data.

--- app/src/main/java/com/thanebuhanan/caloriecounter/data/dto/UserDTO.kt
+++ app/src/main/java/com/thanebuhanan/caloriecounter/data/dto/UserDTO.kt
@@ -7,9 +7,6 @@ import java.util.UUID
 @Entity(tableName = "user")
 data class UserDTO(
     @PrimaryKey val id: String = UUID.randomUUID().toString(),
-    val age: Int,
-    val heightFt: Int,
-    val heightIn: Int,
     val weight: Int,
     val hasGoalGain: Boolean,
 )
