03ff15d59bf2db5686c37a173c687da4250b439a
ThaneBuhanan <thanebuhanan@gmail.com>
2023-05-18 15:22:40 -0700

    notifications almost complete.

---
+++ app/src/main/java/com/thanebuhanan/caloriecounter/utils/NotificationUtils.kt
@@ -0,0 +1,47 @@
+package com.thanebuhanan.caloriecounter.utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.thanebuhanan.caloriecounter.MainActivity
+import com.thanebuhanan.caloriecounter.R
+
+private val NOTIFICATION_ID = 0
+
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.bicep
+    )
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
+
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.notification_channel_id)
+    )
+        .setSmallIcon(R.drawable.bicep)
+        .setContentTitle(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
+        .setPriority(NotificationCompat.PRIORITY_MIN)
+
+    notify(NOTIFICATION_ID, builder.build())
+}
